854895afa937cd773d7ab1d43d5de7ccbd4bf897
Carrie <carriescott0@gmail.com>
2020-09-13 16:10:50 +0100

    remove form from screen when user clicks 'Compare Me!'

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 
+
+(function () {
+//    Set global variable constants
+    const subBtn = document.getElementById('btn');
+    const form = document.getElementById('dino-compare');
+
+
+
+    // Add click event listener to submit button to trigger event
+    subBtn.addEventListener('click', () => {
+        console.log('hello');
+        form.classList.add('hide');
+    });
+//    Remove form from screen
+
+
+
+
+
+
+}());
+
+
     // Create Dino Constructor
 
 
@@ -11,22 +34,26 @@
 
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
 
 
 // On button click, prepare and display infographic
+    // get data from JSON
+
+
+
